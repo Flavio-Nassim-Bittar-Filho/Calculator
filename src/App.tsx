53b57033a1de77e64d35fa5aa14205e7a160ffd2
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2023-03-25 16:14:47 -0300

    remove axios and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Keyboard } from './components/Keyboard'
+
 export function App() {
   return (
-    <div className='w-full h-full flex flex-col justify-center items-center bg-[url("src/assets/img/world.jpg")] bg-cover bg-center'>
-      <div className='text-8xl text-blue-200'>Hello World</div>
+    <div className='w-full h-full flex flex-col justify-center items-center'>
+      <Keyboard></Keyboard>
     </div>
   )
 }
